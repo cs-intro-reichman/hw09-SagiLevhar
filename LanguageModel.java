be40ace3c67ddb1fb57df3895fe1bbfeb3e6562f
SagiLevhar <sagi.levhar@post.runi.ac.il>
2024-02-28 23:10:37 +0200

    HW09

--- LanguageModel.java
+++ LanguageModel.java
@@ -3,6 +3,8 @@ import java.util.Random;
 
 public class LanguageModel {
 
+
+
     // The map of this model.
     // Maps windows to lists of charachter data objects.
     HashMap<String, List> CharDataMap;
@@ -33,18 +35,84 @@ public class LanguageModel {
 
     /** Builds a language model from the text in the given file (the corpus). */
 	public void train(String fileName) {
-		// Your code goes here
+
+        String window = "";
+        char c;
+        In in = new In(fileName);
+
+        // Reads just enough characters to form the first window
+        for (int i = 0; i < windowLength; i++) {
+            window += in.readChar();
+        }
+
+        // Processes the entire text, one character at a time
+        while (!in.isEmpty()) {
+            // Gets the next character
+            c = in.readChar();
+
+            // Checks if the window is already in the map
+            List probs = CharDataMap.get(window);
+
+            // If the window was not found in the map
+            if (probs == null) {
+                // Creates a new empty list, and adds (window,list) to the map
+                probs = new List();
+                CharDataMap.put(window, probs);
+            }
+            // Calculates the counts of the current character.
+            probs.update(c);
+
+            // Advances the window: adds c to the window’s end, and deletes the
+            // window's first character.
+            window += c;
+            window = window.substring(1); 
+        }
+
+        // The entire file has been processed, and all the characters have been counted.
+        // Proceeds to compute and set the p and cp fields of all the CharData objects
+        // in each linked list in the map.
+        for (List probs : CharDataMap.values())
+            calculateProbabilities(probs);
 	}
 
     // Computes and sets the probabilities (p and cp fields) of all the
 	// characters in the given list. */
 	public void calculateProbabilities(List probs) {				
 		// Your code goes here
+        
+        int totalCharacters = 0;
+        
+        ListIterator itr = probs.listIterator(0);
+        while (itr.hasNext()) {
+            CharData charData = itr.next(); // Get next CharData object
+            totalCharacters += charData.count; // Accumulate character count
+        }
+
+        double counter = 0;
+        itr = probs.listIterator(0);
+        while(itr.hasNext()) {
+            CharData charData = itr.next();
+            charData.p = ((double) charData.count) / totalCharacters;
+            counter += charData.p;
+            charData.cp = counter;
+        }
 	}
 
     // Returns a random character from the given probabilities list.
 	public char getRandomChar(List probs) {
 		// Your code goes here
+        double r = randomGenerator.nextDouble();
+        ListIterator itr = probs.listIterator(0);
+
+        while (itr.hasNext()) {
+            CharData charData = itr.next();
+            
+            if (charData.cp > r) {
+                return charData.chr;
+            }
+        }
+        // If no character is selected, return the last character in the list
+        return probs.get(probs.getSize() - 1).chr;
 	}
 
     /**
@@ -56,6 +124,43 @@ public class LanguageModel {
 	 */
 	public String generate(String initialText, int textLength) {
 		// Your code goes here
+        
+        /*if (initialText.length() < windowLength) {
+            return initialText;
+        }
+
+        String window = initialText.substring(Math.max(0, initialText.length() - windowLength));
+        StringBuilder randomText = new StringBuilder(initialText);
+        char c;
+        while (randomText.length() - initialText.length() < textLength) {
+
+            List probs = CharDataMap.get(window);
+            if (probs == null) {
+                return randomText.toString();
+            }
+            c = getRandomChar(probs);
+            randomText.append(c);
+            window = window.substring(1) + c; 
+        }
+
+        return randomText.toString();*/
+
+        if (initialText.length() < windowLength) {
+            return initialText;
+        }
+    
+        String window = initialText.substring(initialText.length() - windowLength);
+        String text = window;
+        while (text.length() - initialText.length() < textLength) {
+            List curr = CharDataMap.get(window);
+            if (curr == null) {
+                return text;
+            }
+            char nextChar = getRandomChar(curr);
+            text += nextChar;
+            window = window.substring(1) + nextChar; // Update window by removing the first character
+        }
+        return text;
 	}
 
     /** Returns a string representing the map of this language model. */
@@ -70,5 +175,20 @@ public class LanguageModel {
 
     public static void main(String[] args) {
 		// Your code goes here
+        int windowLength = Integer.parseInt(args[0]);
+        String initialText = args[1];
+        int generatedTextLength = Integer.parseInt(args[2]);
+        Boolean randomGeneration = args[3].equals("random");
+        String fileName = args[4];
+
+        LanguageModel lm;
+        if (randomGeneration) {
+            lm = new LanguageModel(windowLength);
+        } else{
+            lm = new LanguageModel(windowLength, 20);
+        }
+
+        lm.train(fileName);
+        System.out.println(lm.generate(initialText, generatedTextLength));
     }
 }
--- List.java
+++ List.java
@@ -30,11 +30,22 @@ public class List {
     /** GIVE Adds a CharData object with the given character to the beginning of this list. */
     public void addFirst(char chr) {
         // Your code goes here
+        CharData c = new CharData(chr);
+        Node newN = new Node(c, first);
+        first = newN;
+        size++;
     }
     
     /** GIVE Textual representation of this list. */
     public String toString() {
         // Your code goes here
+        Node curr = first;
+        String string = "(";
+        while (curr != null) {
+            string += curr.toString() + " ";
+            curr = curr.next;
+        }
+        return string.substring(0, string.length()) +")";
     }
 
     /** Returns the index of the first CharData object in this list
@@ -42,6 +53,16 @@ public class List {
      *  or -1 if there is no such object in this list. */
     public int indexOf(char chr) {
         // Your code goes here
+        Node curr = first;
+        int index = 0;
+        while (curr != null) {
+            if (curr.cp.chr == chr) {
+                return index;
+            }
+            curr = curr.next;
+            index++;
+        }
+        return -1;
     }
 
     /** If the given character exists in one of the CharData objects in this list,
@@ -49,6 +70,18 @@ public class List {
      *  given chr to the beginning of this list. */
     public void update(char chr) {
         // Your code goes here
+        Node curr = first;
+        int i = indexOf(chr);
+        if (i != -1) {
+            int j = 0;
+            while (j<i) {
+                curr = curr.next;
+                j++;
+            }
+            curr.cp.count++;
+        } else{
+            addFirst(chr);
+        }
     }
 
     /** GIVE If the given character exists in one of the CharData objects
@@ -56,13 +89,43 @@ public class List {
      *  true. Otherwise, returns false. */
     public boolean remove(char chr) {
         // Your code goes here
+        Node prev = null;
+        Node curr = first;
+        while (curr != null && !curr.cp.equals(chr)) {
+            prev = curr;
+            curr = curr.next;
+        }
+        if (curr == null) {
+            return false;
+        }
+        if (prev == null) {
+            first = first.next;
+        } else{
+            prev.next = curr.next;
+        }
+        size--;
+        return true;
     }
+
 
     /** Returns the CharData object at the specified index in this list. 
      *  If the index is negative or is greater than the size of this list, 
      *  throws an IndexOutOfBoundsException. */
     public CharData get(int index) {
         // Your code goes here
+        if (index >= size || index<0) {
+            throw new IndexOutOfBoundsException("illegal index" + index);
+        }
+        Node curr = first;
+        int i = 0;
+        while (i<index && curr!= null) {
+            curr = curr.next;
+            i++;
+        }
+        if (i == index) {
+            return curr.cp;
+        }
+        return null;
     }
 
     /** Returns an array of CharData objects, containing all the CharData objects in this list. */
